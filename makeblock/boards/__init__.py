b907df377aad7d68dfc93a1972efaecc082f4ee9
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2025-03-24 23:30:47 +0000

    patch: disabled bugged import

--- makeblock/boards/__init__.py
+++ makeblock/boards/__init__.py
@@ -9,7 +9,7 @@ from . import megapipro as MegaPiPro
 from . import meorion as MeOrion
 from . import mcore as mCore
 from . import codey as Codey
-from . import cyberpi as CyberPi
+# from . import cyberpi as CyberPi
 from . import rj25n as RJ25N
 mcore = mCore.__mCore()
 mbuild = mBuild.__mBuild()
@@ -18,4 +18,5 @@ meorion = MeOrion.__MeOrion()
 meauriga = MeAuriga.__MeAuriga()
 megapi = MegaPi.__MegaPi()
 megapipro = MegaPiPro.__MegaPiPro()
-cyberpi = CyberPi.__CyberPi().api
+# --- BUGGED IMPORT ---
+# cyberpi = CyberPi.__CyberPi().api
